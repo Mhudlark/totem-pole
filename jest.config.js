18727138665c2dd21c1662695bba08517eaba09d
Hugh Henry <hughcthenry@gmail.com>
2022-11-14 22:01:33 +1100

    chore: Disable jest code coverage

--- jest.config.js
+++ jest.config.js
@@ -14,21 +14,21 @@ const customJestConfig = {
   },
   setupFilesAfterEnv: ['./jest.setup.js'],
   clearMocks: true,
-  collectCoverage: true,
-  collectCoverageFrom: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    '!./src/**/_*.{js,jsx,ts,tsx}',
-    '!**/*.d.ts',
-    '!**/node_modules/**',
-  ],
-  coverageThreshold: {
-    global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
-    },
-  },
+  // collectCoverage: true,
+  // collectCoverageFrom: [
+  //   './src/**/*.{js,jsx,ts,tsx}',
+  //   '!./src/**/_*.{js,jsx,ts,tsx}',
+  //   '!**/*.d.ts',
+  //   '!**/node_modules/**',
+  // ],
+  // coverageThreshold: {
+  //   global: {
+  //     branches: 80,
+  //     functions: 80,
+  //     lines: 80,
+  //     statements: 80,
+  //   },
+  // },
   testEnvironment: 'jest-environment-jsdom',
 };
 
