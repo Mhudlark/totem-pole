323f6c187090f76056260e93f6285f428cabc1eb
Hugh Henry <hughcthenry@gmail.com>
2022-11-14 22:34:11 +1100

    chore: Rename about page to lobby page

--- src/pages.test/lobby.test.tsx
+++ src/pages.test/lobby.test.tsx
@@ -1,18 +1,18 @@
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 
-import About from '@/pages/about';
+import Lobby from '@/pages/lobby';
 import store from '@/store/store';
 
 // The easiest solution to mock `next/router`: https://github.com/vercel/next.js/issues/7479
 // The mock has been moved to `__mocks__` folder to avoid duplication
 
-describe('About page', () => {
+describe('Lobby page', () => {
   describe('Render method', () => {
     it('Test page renders without error', () => {
       render(
         <Provider store={store}>
-          <About />
+          <Lobby />
         </Provider>
       );
 
---
+++ src/pages/lobby.tsx
@@ -0,0 +1,14 @@
+import { Button, Stack } from '@mui/material';
+
+const Lobby = () => (
+  <Stack sx={{ gap: 4 }}>
+    <Button variant="outlined" onClick={() => {}}>
+      Join
+    </Button>
+    <Button variant="outlined" onClick={() => {}}>
+      Host
+    </Button>
+  </Stack>
+);
+
+export default Lobby;
