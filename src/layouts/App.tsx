c19fbee507ad88280d934d8786d7e1996012e309
Hugh Henry <hughcthenry@gmail.com>
2022-11-15 23:21:00 +1100

    feat: Update app layout with padding

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -1,3 +1,5 @@
+import { Container } from '@mui/material';
+
 import SnackBar from '@/components/SnackBar';
 
 import { Meta } from './Meta';
@@ -13,7 +15,11 @@ const App = ({ children }: AppLayoutProps) => {
         title="Next.js Boilerplate Presentation"
         description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
       />
-      {children}
+      <Container
+        sx={{ px: { xs: 2, sm: 3, md: 4 }, py: { xs: 4, sm: 6, md: 8 }, m: 0 }}
+      >
+        {children}
+      </Container>
       <SnackBar />
     </>
   );
