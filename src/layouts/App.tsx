5a2d38a96cb967b751557d3bddb8a56cb20a9209
Hugh Henry <hughcthenry@gmail.com>
2022-11-16 14:18:42 +1100

    feat: Update global page title and description

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -11,10 +11,7 @@ type AppLayoutProps = {
 const App = ({ children }: AppLayoutProps) => {
   return (
     <>
-      <Meta
-        title="Next.js Boilerplate Presentation"
-        description="Next js Boilerplate is the perfect starter code for your project. Build your React application with the Next.js framework."
-      />
+      <Meta title="Totem Pole" description="Totem Pole" />
       <Container
         sx={{ px: { xs: 2, sm: 3, md: 4 }, py: { xs: 4, sm: 6, md: 8 }, m: 0 }}
       >
