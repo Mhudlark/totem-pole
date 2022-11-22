479375cc0eb96b6006b078b7ae7e5067bb8ae9dd
Hugh Henry <hughcthenry@gmail.com>
2022-11-22 16:50:19 +1100

    chore: Update init user function for updated user type

--- src/sharedUtils/user.ts
+++ src/sharedUtils/user.ts
@@ -3,5 +3,8 @@ import type { User } from '@/sharedTypes';
 export type InitUser = (username: string) => User;
 
 export const initUser: InitUser = (username) => ({
-  username,
+  userMetadata: {
+    key: Math.random().toString(36).slice(2, 9),
+    username,
+  },
 });
